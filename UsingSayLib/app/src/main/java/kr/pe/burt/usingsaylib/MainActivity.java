3e1c6a227993594a36b414ccaf26dc6fe5e040ff
burt <skyfe79@gmail.com>
2015-03-21 11:12:00 +0900

    add a project that uses saylib.aar library

---
+++ UsingSayLib/app/src/main/java/kr/pe/burt/usingsaylib/MainActivity.java
@@ -0,0 +1,47 @@
+package kr.pe.burt.usingsaylib;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import kr.pe.burt.android.saylib.library.Say;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Say say = new Say();
+        TextView tvHello = (TextView)findViewById(R.id.tvHello);
+        tvHello.setText(say.hello(getResources()));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
